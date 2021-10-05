abce6b6a07a4252341b1b34bb6249a0bc15dcb8f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-05 15:59:55 +0100

    add repos

--- src/main/java/de/tekup/db/entities/EmployeeEntity.java
+++ src/main/java/de/tekup/db/entities/EmployeeEntity.java
@@ -3,12 +3,16 @@ package de.tekup.db.entities;
 import java.time.LocalDate;
 
 import javax.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name="employee")
+@Data
 public class EmployeeEntity {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	@Column(length=50)
 	private String name;
 	@Column(length=70, nullable=false, unique=true)
