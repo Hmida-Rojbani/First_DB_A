e3fc1968a814e5e53c15880b54e6909d756feb4d
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-05 16:03:40 +0100

    Create EmployeeCtrl.java

---
+++ src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
@@ -0,0 +1,22 @@
+package de.tekup.db.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tekup.db.entities.EmployeeEntity;
+import de.tekup.db.services.EmployeeService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class EmployeeCtrl {
+	
+	private EmployeeService empService;
+	
+	@PostMapping("/employee/add")
+	public EmployeeEntity saveEmp(@RequestBody EmployeeEntity employee) {
+		return empService.saveToDB(employee);
+	}
+
+}
