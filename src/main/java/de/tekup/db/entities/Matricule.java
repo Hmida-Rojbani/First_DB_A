625af46badc5ed15b1ef5d6371c08c7c832364d9
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-12 15:43:16 +0100

    Update Matricule.java

--- src/main/java/de/tekup/db/entities/Matricule.java
+++ src/main/java/de/tekup/db/entities/Matricule.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
@@ -24,6 +26,7 @@ public class Matricule {
 	
 	private String creator;
 	
+	@JsonIgnore
 	@OneToOne(mappedBy = "matricule")
 	private EmployeeEntity employee;
 	
