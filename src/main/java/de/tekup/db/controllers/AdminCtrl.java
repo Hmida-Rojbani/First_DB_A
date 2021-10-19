75c5b724505d60663048ba10c5713dbb917241a0
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-19 13:56:37 +0100

    add matricule

--- src/main/java/de/tekup/db/controllers/AdminCtrl.java
+++ src/main/java/de/tekup/db/controllers/AdminCtrl.java
@@ -3,11 +3,13 @@ package de.tekup.db.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.tekup.db.entities.Admin;
+import de.tekup.db.entities.Matricule;
 import de.tekup.db.services.AdminService;
 import lombok.AllArgsConstructor;
 
@@ -27,5 +29,10 @@ public class AdminCtrl {
 		return adminService.getAllAdmins();
 	}
 	
+	@PostMapping("/admins/{id}/add/matricule")
+	public Matricule addMatriculeToAdmin(@PathVariable int id,
+			@RequestBody Matricule matricule) {
+		return adminService.addMatriculeToAdmin(id, matricule);
+	}
 
 }
--- src/main/java/de/tekup/db/services/AdminService.java
+++ src/main/java/de/tekup/db/services/AdminService.java
@@ -1,12 +1,14 @@
 package de.tekup.db.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
 import de.tekup.db.entities.Admin;
 import de.tekup.db.entities.Matricule;
 import de.tekup.db.repositories.AdminRepository;
+import de.tekup.db.repositories.MatriculeRepository;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -14,6 +16,7 @@ import lombok.AllArgsConstructor;
 public class AdminService {
 	
 	private AdminRepository adminRepos;
+	private MatriculeRepository matRepos;
 	
 	public Admin saveToDB(Admin admin) {
 		adminRepos.save(admin);
@@ -30,5 +33,24 @@ public class AdminService {
 	public List<Admin> getAllAdmins(){
 		return adminRepos.findAll();
 	}
-
+	
+	public Admin getAdminById(int id){
+		return adminRepos.findById(id)
+		.orElseThrow(()-> new NoSuchElementException("Admin id not found"));
+	}
+	
+	// add of matricule to an admin 
+	//locate admin by id
+	// admin ->  list of matricule
+	// new matricule contain the admin reference 
+	// add new matricule to that list
+	// save Matricule
+	
+	public Matricule addMatriculeToAdmin(int id, Matricule matricule) {
+		Admin admin = getAdminById(id);
+		matricule.setCreator(admin);
+		admin.getMatricules().add(matricule);
+		matRepos.save(matricule);
+		return matricule;
+	}
 }
